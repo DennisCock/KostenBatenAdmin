704bc9f2ab4f477ac3540a86580bb275beac5502
Joren_Msi <Joren_Msi@MSI>
2017-03-09 09:55:05 +0100

    implementeren klassendiagram + start FXML v2

--- src/domein/gebruikers/JobCoach.java
+++ src/domein/gebruikers/JobCoach.java
@@ -2,12 +2,12 @@ package domein.gebruikers;
 
 import domein.Adres;
 
-public class JobCoach {
+public class JobCoach extends Gebruiker{
     private String naamOrganisatie;
     private Adres adresOrganisatie;
     
     
-    public JobCoach() {
+    public JobCoach(){
     }
     
 }
